2aedb8f7abdb132f2f2f9fd80c659f16279300fd
Karina Finn <karina.finn@kellogg.northwestern.edu>
2024-03-07 04:01:37 +0000

    wip

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,24 @@ class SessionsController < ApplicationController
   end
 
   def create
-  end
-
-  def destroy
+  @user = User.find_by({ "username" => params["username"] })
+  if @user != nil
+    if BCrypt::Password.new(@user["password"]) == params["password"]
+      session["user_id"] = @user["id"]
+      redirect_to "/"
+    else
+      flash["notice"] = "Invalid username or password."
+      redirect_to "/login"
+    end
+  else
+    flash["notice"] = "Try again."
+    redirect_to "/login"
   end
 end
-  
+
+  def destroy
+    session["user_id"] = nil
+    flash["notice"] = "Goodbye!"
+    redirect_to "/login"
+  end
+end
